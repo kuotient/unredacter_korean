0279e6e5febc2324721661134f1fd53be5519566
Ji soo Kim <kuotient.dev@gmail.com>
2022-03-10 16:48:26 +0900

    ADD Image class, TEST generate_text function

---
+++ test/unittest_text_image.py
@@ -0,0 +1,35 @@
+import unittest
+
+from util.text_image import TextParameters
+from util.text_image import generate_image_from_text, pixelize_image
+#from params import TextImageParameters, PixelImageParameters
+
+
+class GenerateTextImages(unittest.TestCase):
+    '''
+    image.py 에 대한 유닛 테스트 케이스
+    '''
+    def test_generate_text_images(self):
+        '''
+        blah
+        '''
+        # parameters
+        text = '동해물과 백두산이'
+        font = 'data/fonts/NanumBarunGothic.ttf'
+        font_size = 12
+        bg_color = (255, 255, 255)
+        offsets = (0, 0)
+
+        text_parameters: TextParameters = TextParameters(
+            font=font,
+            font_size=font_size,
+            bg_color=bg_color,
+            offsets=offsets)
+
+        text_image = generate_image_from_text(text, text_parameters)
+        text_image.image.show()
+        self.assertEqual(text_image.text, text)
+
+
+if __name__ == '__main__':
+    unittest.main()
